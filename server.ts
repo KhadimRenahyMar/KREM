7e0623909a48c5a7b1a3e47f66fbb71d8afa4af6
ZIMA <dovahkrm@gmail.com>
2022-06-15 22:29:25 +0200

    chore: set different env variable for dev/prod

--- server.ts
+++ server.ts
@@ -7,18 +7,18 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 
 if(process.env.NODE_ENV === "production") {
+    console.log(process.env.NODE_ENV);
     server.use(express.static(path.join(__dirname, 'client/build')));
-    
 }
-else{
-    server.use(express.static(path.join(__dirname, 'client/build')));
+else {
+    console.log(process.env.NODE_ENV);
+    // server.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //     next();
+    // });
 }
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});  
 server.use(router);
 // server.use((req, res) =>{
 //     res.status(404).redirect('/404');
