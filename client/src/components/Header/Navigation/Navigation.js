f001f4b066df4b33c5c82c26fb10b56b27686dae
ZIMA <dovahkrm@gmail.com>
2022-06-27 12:59:31 +0200

    chore: added svg to public folder ; feat : landing page first elements layed out

---
+++ client/src/components/Header/Navigation/Navigation.js
@@ -0,0 +1,25 @@
+import './Navigation.scss';
+import { NavLink } from 'react-router-dom';
+
+export default function Navigation(){
+    return (
+        <nav className='nav'>
+            <NavLink
+            to='/' className='nav__link'>
+                Acceuil
+            </NavLink>
+            <NavLink
+            to='/projects' className='nav__link'>
+                Projets
+            </NavLink>
+            <NavLink
+            to='/skills' className='nav__link'>
+                Compétences
+            </NavLink>
+            <NavLink
+            to='/contact' className='nav__link'>
+                Contact
+            </NavLink>
+        </nav>
+    );
+};
