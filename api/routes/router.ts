a5b2986d70f01b326e5ca8c4f5a0b173a6ae3c54
ZIMA <dovahkrm@gmail.com>
2023-09-12 00:51:07 +0200

    bugfix: added / in api's url

--- api/routes/router.ts
+++ api/routes/router.ts
@@ -3,9 +3,10 @@ import mainController from "../controllers/mainController";
 import projectController from "../controllers/projectController";
 import techController from "../controllers/techController";
 const router = Router();
-router.get('api/projects/all', projectController.getAllProjects);
-router.get('api/projects/:projectId', projectController.getProject);
-router.post('api/techs', techController.getAllTechs);
-router.get('api/userInfos', mainController.getUserInfo);
-router.get('api/getGifs', mainController.getGifs);
+
+router.get('/api/projects/all', projectController.getAllProjects);
+router.get('/api/projects/:projectId', projectController.getProject);
+router.post('/api/techs', techController.getAllTechs);
+router.get('/api/userInfos', mainController.getUserInfo);
+router.get('/api/getGifs', mainController.getGifs);
 export default router;
